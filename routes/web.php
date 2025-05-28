42647392175a81438aa3008adf73cbb8daa5b1c6
Christiaan Richter <35594132+CrichterNex@users.noreply.github.com>
2025-05-28 13:04:11 +0200

    bootstrap and login functions added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false, 'reset' => false, 'verify' => false, 'remember_me' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
